88f6130986c326d2aa956129303334c477848416
Zalxio <dyl_974@icloud.com>
2021-12-06 22:11:53 +0400

    add map view

---
+++ PollutionProject/Controlleurs/MapVC.swift
@@ -0,0 +1,22 @@
+//
+//  MapVC.swift
+//  PollutionProject
+//
+//  Created by Dylan Cherrier on 06/12/2021.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapVC: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
