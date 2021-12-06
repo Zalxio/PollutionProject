237d6b11569170475861aec237ed06d288c6daca
Zalxio <dyl_974@icloud.com>
2021-12-07 01:25:47 +0400

    add localisation and language

--- PollutionProject/Controlleurs/MapVC.swift
+++ PollutionProject/Controlleurs/MapVC.swift
@@ -13,10 +13,69 @@ class MapVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    let managerLoc = CLLocationManager()
+    let zoom: Double = 10000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        mapView.delegate = self
+        // Localisation
+        locationServices()
     }
     
+    //MARK: Localisation
+    
+    // vérifie si le service de localisation est activer
+    private func locationServices(){
+        if CLLocationManager.locationServicesEnabled(){
+            initLocation()
+            locationAuthorization()
+        }else{
+            
+        }
+    }
+    
+    // initialise la localisation et la position
+    private func initLocation(){
+        managerLoc.delegate = self
+        managerLoc.desiredAccuracy = kCLLocationAccuracyReduced
+    }
+    
+    // vérifie l'authorisation accorder par l'utillisateur pour l'utilisation du sevice de localisation
+    private func locationAuthorization(){
+        switch managerLoc.authorizationStatus{
+        case .authorizedAlways :        // toujours authorise (cas : mise à jour du trajet en arrière plan)
+            break
+        case .authorizedWhenInUse :     // que lorsque l'app est au premier plan et utiliser (cas : localisation)
+            mapView.showsUserLocation = true    // afficher localisation de l'utilisateur
+            mapView.showsCompass = true
+            mapView.setUserTrackingMode(MKUserTrackingMode.follow, animated: true)
+            break
+        case .restricted :              // ex: restraint par un parent le statut ne peut etre modifier
+            break
+        case .denied :                  // app non authoriser a utiliser le service de localisation
+            break
+        case .notDetermined:            // pas encore déterminer
+            break
+        @unknown default:
+            break
+        }
+    }
 
+}
+
+extension MapVC: CLLocationManagerDelegate{
+    // pour mettre à jour la position de l'utilisateur sur la carte
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        locationAuthorization()     // vérifie l'authorisation quand il change
+    }
+}
+
+extension MapVC : MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, didChange mode: MKUserTrackingMode, animated: Bool) {
+        print("change")
+    }
 }
